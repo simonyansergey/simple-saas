df88fc682f9c2883992e0a0232fae7484f636460
simonyansergey <sergey.a.simonyan@gmail.com>
2026-01-19 19:08:42 +0400

    feat: Add Credit logic code skeleton

---
+++ app/Http/Controllers/CreditController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Package;
+use Illuminate\Http\Request;
+
+class CreditController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function buyCredits(Package $package)
+    {
+
+    }
+
+    public function success()
+    {
+
+    }
+
+    public function cancel()
+    {
+
+    }
+
+    public function webhook()
+    {
+
+    }
+}
